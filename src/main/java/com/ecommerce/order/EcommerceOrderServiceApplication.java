9ec1ee2a29bb0c9c8c4555667969bf994930a0fe
sjyu <sjyu@i-leaders.co.kr>
2023-11-27 20:44:02 +0900

    feat : feignClient 추

--- src/main/java/com/ecommerce/order/EcommerceOrderServiceApplication.java
+++ src/main/java/com/ecommerce/order/EcommerceOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class EcommerceOrderServiceApplication {
 
 	public static void main(String[] args) {
